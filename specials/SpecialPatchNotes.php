bd4ecf931fd4ec4447f17a35f52e39ac0b9da6d9
olivermt91 <moliver@centeredgesoftware.com>
2014-09-04 17:50:21 -0400

    0.2.1
    Still not working, but the meat is in there.  I think I'm close.

--- specials/SpecialPatchNotes.php
+++ specials/SpecialPatchNotes.php
@@ -3,20 +3,32 @@ class SpecialPatchNotes extends SpecialPage {
 	function __construct() {
 		parent::__construct( 'MyExtension' );
 	}
- 
+	
 	function execute( $par ) {
 		$request = $this->getRequest();
 		$output = $this->getOutput();
 		$this->setHeaders();
  
-		# Get request data from, e.g.
 		$param = $request->getText( 'param' );
  
-		# Do stuff
-		# ...
-		$array = file('https://upgrades.pfestore.com/advantage/14.2.8.67/Patch/Patch%20Notes.txt');
+		$array = array();
 		
-		$wikitext = $array;
-		$output->addWikiText( $wikitext );
+		$handle = fopen($wgPatchNotesURL, 'r');
+		if ($handle) {
+			while ($line = fgets($handle)) {
+				if (explode(' ', trim($line)) = 'Update') {
+					array_push($array, '== ' + $line + ' ==');
+				} else {
+					array_push($array, '* ' +$line);
+				}
+			}
+		} else {
+			$output->addWikiText( "Something's wrong!" );
+		}
+		fclose($handle);
+		
+		foreach ($array as $value) {
+			$output->addWikiText( $value );
+		}
 	}
 }
